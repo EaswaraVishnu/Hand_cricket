7a40fbe49f2722a64632579101b781701f95181f
Easwara Vishnu S R <132735765+EaswaraVishnu@users.noreply.github.com>
2023-07-08 11:39:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 'use strict'
-//Generating computer choice
+//Generating target score
 let score = 0
+const target = Math.trunc(Math.random()*10+1);
+document.querySelector(".target_text").textContent = `Your target score is ${target}`;
+
 
 const buttons = document.getElementsByClassName("keys");
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function() {
     const value = Number(this.value);
-    display(value);
+    display_score(value);
   });
 }
 
-const display = function(run){
+const display_score = function(run){
     const com_choice = Math.trunc(Math.random()*6+1);
-    run === com_choice ? score = 0 : score += run;
-    document.querySelector(".score").textContent = score;
+    document.querySelector(".computer_score").textContent = com_choice;
+    run === com_choice ? document.querySelector(".target_text").textContent = "Howzaaaat !!!!" : score += run;
+    document.querySelector(".player_score").textContent = score;
+    display_result(score);
 }
 
+const display_result = function(a){
+  if(score >= target){
+    document.querySelector(".target_text").textContent = "You win";
+  }
+  else{
+    document.querySelector(".target_text").textContent = `Need ${target-score} runs to win`;
+  }
+}
 
